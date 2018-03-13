632c2a48ff779fcfe17ccb20c308400e38af7999
Yellow Rain <36498383+BXQC@users.noreply.github.com>
2018-03-13 14:02:01 +0800

    Update M1303.py

--- M1303.py
+++ M1303.py
@@ -8,7 +8,7 @@ for i in range(0,L):
 		print('The {} animal is at {} and is a {}'.format(index[i],i,animals[i]))
 		exit(0)
 	else:
-		print('The {} th animal is at {} and is a {}'.format(i+1,i,animals[i]))
+		print('The {}th animal is at {} and is a {}'.format(i+1,i,animals[i]))
 #我不知道这样写有什么错误...此处留着,先试下只用for的循环嵌套,看可不可以...还是没找到问题所在....感觉break加在哪里都不对,run的时候提示的一直都是语法错误.....先留着,以后会了再回来DEBUG.....后面一篇中看到了exit(0)的用法,用于本篇去无济于事,总之是我还不太会用.先这样,等习惯了用法,再回来改.....
 #.搞定!!!!!!!!!!!!!!!!就是少了一个括号............握草......难受...................
 #'''for i in range(0,L):
